1c8a546294dd0458a92a102d48faa5a375b46d83
rubensb-git <rubens.bento@gmail.com>
2023-01-28 09:18:25 -0300

    Department List design

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -34,7 +34,7 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		System.out.println("Menu item Department");
+		loadView("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
